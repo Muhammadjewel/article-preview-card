21c6ebf5dd50f2c811359845253660378acd47ad
muhammadjewel <msuratov@gmail.com>
2023-09-08 21:34:38 +0500

    Add toggleShareWidget feature

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 function init () {
+  const elShareWidget = document.querySelector('.share-widget');
+  const elShareWidgetToggler = elShareWidget.querySelector('.share-widget__toggler');
 
+  function toggleShareWidget () {
+    elShareWidget.classList.toggle('share-widget--open');
+  }
+
+  if (elShareWidgetToggler) {
+    elShareWidgetToggler.addEventListener('click', toggleShareWidget);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', init);
